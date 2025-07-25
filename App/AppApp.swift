7950a0be339200f2f7013a7a9f97d808ec7d6eaf
Estelle Joubert <estellejoubert067@gmail.com>
2025-07-25 09:30:33 -0400

    Initial Commit

---
+++ App/AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by Scholar on 7/25/25.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
