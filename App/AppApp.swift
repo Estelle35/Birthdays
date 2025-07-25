e58e710b99695aa8333806a2c79fa16050bc0f65
Estelle Joubert <estellejoubert067@gmail.com>
2025-07-25 10:59:02 -0400

    You can save Data

--- App/AppApp.swift
+++ App/AppApp.swift
@@ -6,12 +6,13 @@
 //
 
 import SwiftUI
-
+import SwiftData
 @main
 struct AppApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .modelContainer(for: Friend.self)
         }
     }
 }
--- App/ContentView.swift
+++ App/ContentView.swift
@@ -6,13 +6,13 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
     
-    @State private var friends: [Friend] = [
-        Friend(name:  "Taylor", birthday: .now),
-        Friend(name: "Frank Ocean", birthday: Date(timeIntervalSince1970:0))
-        ]
+    @Query private var friends: [Friend]
+    @Environment(\.modelContext) private var context
+    
     @State private var newName = ""
     @State private var newBirthday = Date.now
     
@@ -22,7 +22,7 @@ struct ContentView: View {
         
         NavigationStack{
             
-            List(friends, id:\.name) {friend in
+            List(friends) {friend in
                 HStack {
                     Text(friend.name)
                     Spacer()
@@ -43,7 +43,7 @@ struct ContentView: View {
 
                     Button("Save") {
                         let newFriend = Friend(name: newName, birthday: newBirthday)
-                        friends.append(newFriend)
+                        context.insert(newFriend)
                         
                         newName = ""
                         newBirthday = .now
@@ -70,4 +70,5 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+        .modelContainer(for: Friend.self, inMemory: true)
 }
